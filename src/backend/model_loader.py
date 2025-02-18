9ceceeb1202c404492859885a61d644b5ff60de0
sss-soham <soham.sakunde@gmail.com>
2025-02-18 13:25:57 +0530

    Model changes

--- src/backend/model_loader.py
+++ src/backend/model_loader.py
@@ -1,13 +1,11 @@
-# model_loader.py
 import gdown
 import os
 import streamlit as st
 from tensorflow import keras
 from pathlib import Path
 
-# Use the file ID instead of folder URL
-FILE_ID = "1w251T9N4mT-_4UVz81QLIfrqVm0mT02d"  # Extract this from your URL
-MODEL_PATH = "./model/mobilenetv2_finetuned.h5"
+# Use secret for file ID
+MODEL_PATH = "model/mobilenetv2_finetuned.h5"
 
 @st.cache_resource
 def load_model():
@@ -17,7 +15,8 @@ def load_model():
     
     if not os.path.exists(MODEL_PATH):
         st.write("Downloading model...")
-        url = f'https://drive.google.com/uc?id={FILE_ID}'
+        file_id = st.secrets["google_drive"]["file_id"]
+        url = f'https://drive.google.com/uc?id={file_id}'
         gdown.download(url, MODEL_PATH, quiet=False)
     
     try:
